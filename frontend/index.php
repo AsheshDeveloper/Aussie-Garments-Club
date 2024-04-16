438af03ae7f7ecbf8d9bd96609ce160bf3798da3
DhakalRupesh <a73dhakal@outlook.com>
2024-04-16 18:36:41 +1000

    [add ] verification when signup, [change] login and signup file folder

--- frontend/index.php
+++ frontend/index.php
@@ -56,9 +56,9 @@ require_once("php/database_connect.php");
                 <!-- Right side content -->
                 <?php if(empty($_SESSION['username'])){ ?>
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="./pages/login.php">Login | </a></li>
+                    <li class="nav-item"><a class="nav-link" href="./pages/authentication/login.php">Login | </a></li>
                     <li class="nav-item">
-                        <a class="nav-link" href="./pages/signup.php">Signup |</a>
+                        <a class="nav-link" href="./pages/authentication/thirdPartySignup.html">Signup |</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="./pages/profile/user_profile.html">Guest</a>
@@ -163,7 +163,7 @@ require_once("php/database_connect.php");
                 <!-- Left Side Content -->
                 <div class="col-md-6 left-content-slider">
                     <h2 class="quote-text mb-4 mt-5">Where style meets convenience, and savings meet satisfaction</h2>
-                    <button class="btn btn-primary me-4 px-5 py-2">Get Started</button>
+                    <a href="./Pages/authentication/signup.php" class="btn btn-primary me-4 px-5 py-2">Get Started</a>
                     <a class="btn btn-outline-primary px-4 py-2" href="./pages/products.html">View Products</a>
                     <p class="mt-2 contacts-info">
                         +610403876990 |
