ad87349069f1ea77315cf000bd19d6c81e95ec57
Ashesh Pathak <ashesh200@gmail.com>
2024-05-25 22:13:46 +1000

    logs implemented

--- frontend/php/auth/auth_login.php
+++ frontend/php/auth/auth_login.php
@@ -1,5 +1,6 @@
 <?php 
 if(isset($_POST['submit'])){
+    require_once "../../php/function/log.php";
     // Connect to the database 
     $connect = mysqli_connect('localhost', 'root', '', 'the_garments_club');
     
@@ -16,7 +17,12 @@ if(isset($_POST['submit'])){
         $_SESSION['userId'] = $row['id'];
         $_SESSION['username'] = $row['first_name'];
         $_SESSION['email'] = $row['email'];
-        if($row['verified']) {
+        if($row['verified']) {            
+            // login activity
+            $userId = $_SESSION['userId']; // Assuming you have stored user ID in session
+            $action = "Login";
+            $actionDetails = "User logged in";
+            logActivity($userId, $action, $actionDetails);
             header('Location: ../../index.php');
         } else {
             header('Location: ../../pages/authentication/verification.php');
